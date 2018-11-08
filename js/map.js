a1f76fefa0688e0d9ea536c05cf082050e48aa5c
JamesMudidi <39102955+JamesMudidi@users.noreply.github.com>
2018-11-09 00:26:19 +0300

    Create map.js

---
+++ js/map.js
@@ -0,0 +1,43 @@
+// google map
+
+function initMap() {
+  var kampala = {
+    lat: 0.3476,
+    lng: 32.5825
+  };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 10,
+    center: kampala
+  });
+  var marker = new google.maps.Marker({
+    position: kampala,
+    map: map
+  });
+}
+
+
+//dropdown
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropdown = document.getElementById("myDropdown");
+    if (myDropdown.classList.contains('show')) {
+      myDropdown.classList.remove('show');
+    }
+  }
+}
+
+function upFunction() {
+  document.getElementById("myDropup").classList.toggle("show");
+}
+window.onclick = function (e) {
+  if (!e.target.matches('.dropbtn')) {
+    var myDropup = document.getElementById("myDropup");
+    if (myDropup.classList.contains('show')) {
+      myDropup.classList.remove('show');
+    }
+  }
+}
